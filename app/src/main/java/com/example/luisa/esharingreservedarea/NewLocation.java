fc7b3fe07674da5480f2ff74caaad3045774ec53
PiersantiLuisa <luisa.piersanti@studenti.unipg.it>
2018-10-28 19:27:14 +0100

    final changes

--- app/src/main/java/com/example/luisa/esharingreservedarea/NewLocation.java
+++ app/src/main/java/com/example/luisa/esharingreservedarea/NewLocation.java
@@ -58,7 +58,7 @@ public class NewLocation extends AppCompatActivity {
 
 
         // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+      //  RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
         String url = "http://carsharingap.000webhostapp.com/server/api/basement/create.php";
 
 
